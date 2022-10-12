e00fcf9bc7cac2234f53638c661f521dcf4dadc6
llamatar <joshuacheung777@gmail.com>
2022-10-11 22:10:35 -0700

    Update script

--- BlockTube_advanced_blocking_watched_videos.js
+++ BlockTube_advanced_blocking_watched_videos.js
@@ -1,20 +1,29 @@
 // Block watched videos in certain pages
 (video, objectType) => {
-  const hideVideosWatchedMoreThanPercentage = 90; 
+  //console.log(video);
   
-  // Block only in certain pages
+  const hideVideosWatchedMoreThanPercentage = 80; 
+  
+  // Do not block videos in pages other than blockedPathnames
   const blockedPathnames = [
     /^\/$/                      // homepage
     , /\/feed\/subscriptions/   // subscriptions
-    , /\/watch/                 // recommendations side panel
+    , /\/watch/                 // related videos sidebar
   ];
-  if (!blockedPathnames.some(p => p.test(window.location.pathname)))
+  if (!blockedPathnames.some(blockedPathname => blockedPathname.test(window.location.pathname)))
     return false;
   
-  // Block only watched videos
+  // Do not block unwatched videos
   if (video.percentWatched === undefined || parseInt(video.percentWatched) < hideVideosWatchedMoreThanPercentage)
     return false;
   
+  // Do not block videos in playlists
+  if (objectType === "playlistPanelVideoRenderer")
+    return false;
+  
   console.log(`BlockTube custom blocking function: "${video.title}" (${video.percentWatched}% watched)`);
+  //console.log(video);
+  //console.log(objectType);
+  //console.log(window);
   return true;
 }
