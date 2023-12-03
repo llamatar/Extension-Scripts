c5f1de671f920abcf0c18d26707e3ce0859fba5f
llamatar <joshuacheung777@gmail.com>
2023-12-03 02:32:54 -0800

    Update script

--- BlockTube_advanced_blocking_watched_videos.js
+++ BlockTube_advanced_blocking_watched_videos.js
@@ -1,29 +1,54 @@
-// Block watched videos in certain pages
 (video, objectType) => {
+  const blockVideosWithLessThanViews = 100;
+  const blockVideosWatchedMoreThanPercentage = 75;
+  const allowVideosPublishedYearsAgo = 10;
+
   //console.log(video);
   
-  const hideVideosWatchedMoreThanPercentage = 80; 
-  
-  // Do not block videos in pages other than blockedPathnames
-  const blockedPathnames = [
-    /^\/$/                      // homepage
-    , /\/feed\/subscriptions/   // subscriptions
-    , /\/watch/                 // related videos sidebar
+  // Allow videos in all pages other than blockablePages
+  const blockablePages = [
+    /^\/$/                      // homepage (exactly "/")
+    , /\/feed\/subscriptions/   // subscriptions (contains "/feed/subscriptions")
+    , /\/watch/                 // related videos sidebar (contains "/watch")
   ];
-  if (!blockedPathnames.some(blockedPathname => blockedPathname.test(window.location.pathname)))
+  if (!blockablePages.some(page => page.test(window.location.pathname)))
     return false;
   
-  // Do not block unwatched videos
-  if (video.percentWatched === undefined || parseInt(video.percentWatched) < hideVideosWatchedMoreThanPercentage)
+  // Block recommended videos with few views, even if unwatched
+  if (/\/watch/.test(window.location.pathname) 
+      && typeof video.viewCount !== "undefined" 
+      && (isNaN(video.viewCount) || video.viewCount < blockVideosWithLessThanViews)) {
+    console.log(`BlockTube: ${video.viewCount} < ${blockVideosWithLessThanViews} views`, video);
+    return true;
+  }
+  
+  // Allow unwatched videos
+  if (video.percentWatched === undefined || parseInt(video.percentWatched) <= blockVideosWatchedMoreThanPercentage)
     return false;
   
-  // Do not block videos in playlists
+  // Allow live livestreams
+  if (video.hasOwnProperty("badges") && video.badges.includes("live"))
+    return false;
+  
+  // Allow published livestreams
+  if (video.publishTimeText.indexOf("Streamed") != -1)
+    return false;
+  
+  // Allow videos in playlists
   if (objectType === "playlistPanelVideoRenderer")
     return false;
   
-  console.log(`BlockTube custom blocking function: "${video.title}" (${video.percentWatched}% watched)`);
+  // Allow videos published many years ago
+  if (video.hasOwnProperty("publishTimeText") 
+      && /year/.test(video.publishTimeText) 
+      && video.publishTimeText.split(" ")[0] >= allowVideosPublishedYearsAgo)
+    return false;
+  
+  console.log(`BlockTube: ${video.percentWatched}% > ${blockVideosWatchedMoreThanPercentage}% watched`, video);
   //console.log(video);
   //console.log(objectType);
   //console.log(window);
+  
+  // Block everything else
   return true;
 }
