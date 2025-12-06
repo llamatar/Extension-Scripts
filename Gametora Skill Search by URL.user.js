2dfbb8307a5c01b04c9a12726caf81b9748b1c6a
llamatar <23285618+llamatar@users.noreply.github.com>
2025-12-06 03:25:27 -0800

    Add Gametora Skill Search by URL userscript

---
+++ Gametora Skill Search by URL.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Gametora Skill Search by URL
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Automatically filters the Umamusume skills page by a URL parameter and opens its tooltip.
+// @author       llamatar
+// @match        https://gametora.com/umamusume/skills*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTerm = urlParams.get('search');
+
+    if (!searchTerm) return;
+
+    function search(searchTerm) {
+        const inputElement = document.querySelector('input[placeholder*="Search by skill name"]');
+        inputElement.value = searchTerm;
+
+        const reactProp = inputElement[Object.keys(inputElement).find(k => k.startsWith('__reactProps'))];
+        reactProp.onChange({target: inputElement, currentTarget: inputElement, type: 'change', bubbles: true});
+
+        setTimeout(() => {
+            const intervalId = setInterval(() => {
+                const visibleSkills = document.querySelectorAll('[class*="skills_table_row_ja__"]:not([class*="skills_hidden__"])');
+
+                if (visibleSkills.length > 0) {
+                    const lastMore = visibleSkills[visibleSkills.length - 1].lastChild?.lastChild?.lastChild;
+                    lastMore.click();
+
+                    clearInterval(intervalId);
+                    setTimeout(adjustTooltip, 300);
+                }
+            }, 100);
+        }, 100);
+    }
+
+    function adjustTooltip() {
+        const tooltipContainer = document.querySelector('[data-tippy-root]');
+        tooltipContainer.style.left = '50%';
+        tooltipContainer.style.transform = 'translate3d(-50%, 0px, 0px)';
+
+        const contentBox = document.querySelector('.tippy-content');
+        contentBox.style.maxHeight = '100vh';
+        contentBox.style.overflowY = 'auto';
+
+        const innerContent = document.querySelector('.tooltips_tooltip__NxFYo');
+        innerContent.style.maxHeight = '100%';
+    }
+
+    search(searchTerm);
+})();
